b02b5fb17bb5d252cbca0ec71e6cc1c9a1fff4bc
huangchen <huangchen_123@qq.com>
2024-10-18 17:31:30 +0800

    feat: 添加setting, models, util等模块

---
+++ gin-blog/models/models.go
@@ -0,0 +1,46 @@
+package models
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	"myProject/gin-blog/setting"
+)
+
+var db *gorm.DB
+
+type Model struct {
+	ID         int `gorm:"primary_key" json:"id"`
+	CreatedOn  int `json:"created_on"`
+	ModifiedOn int `json:"modified_on"`
+}
+
+func init() {
+	sec, err := setting.Cfg.GetSection("database")
+	if err != nil {
+		log.Fatal(2, "Fail to get section 'database': %v", err)
+	}
+
+	// dbType := sec.Key("TYPE").String()
+	dbName := sec.Key("NAME").String()
+	user := sec.Key("USER").String()
+	password := sec.Key("PASSWORD").String()
+	host := sec.Key("HOST").String()
+	// tablePrefix := sec.Key("TABLE_PREFIX").String()
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, host, dbName)
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Println()
+	}
+
+	mysqlDB, _ := db.DB()
+	// SetMaxIdleConns sets the maximum number of connections in the idle connection pool.
+	mysqlDB.SetMaxIdleConns(10)
+
+	// SetMaxOpenConns sets the maximum number of open connections to the database.
+	mysqlDB.SetMaxOpenConns(100)
+}
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"myProject/gin-blog/setting"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	router := gin.Default()
+	router.GET("/test", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": "test"})
+	})
 
+	server := http.Server{
+		Addr:           fmt.Sprintf(":%d", setting.HttpPort),
+		Handler:        router,
+		ReadTimeout:    setting.ReadTimeout,
+		WriteTimeout:   setting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	server.ListenAndServe()
 }
